7d3d15d7ae8704df7ef9a7a0dc603808dbaf9485
galu7424 <galu7424@colorado.edu>
2021-04-06 13:15:15 -0700

    /hello is from tutorial, /app is target environemnt

--- app/settings.py
+++ app/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "gettingstarted.urls"
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "gettingstarted.wsgi.application"
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
